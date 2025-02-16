94b8fe23fdcfa184216847d2d77fa6c28b2111a6
cpesar <cpesar1@gmail.com>
2025-02-15 21:40:06 -0700

    fixed builds

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,25 +1,31 @@
-"use client";
+// "use client";
 
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
-const SnippetNotFoundPage = () => {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center gap-4">
-        <div className="p-6  rounded-lg shadow-md text-center">
-          <h1 className="text-3xl font-bold mb-4">Not Found</h1>
-          <p className="text-destructive">Could not find requested page</p>
-          <Button
-            variant="outline"
-            className="mt-4 ml-2 "
-            onClick={() => (window.location.href = "/")}
-          >
-            Back to Home
-          </Button>
-        </div>
-      </div>
-    </div>
-  );
+// const SnippetNotFoundPage = () => {
+//   return (
+//     <div className="flex flex-col items-center justify-center min-h-screen">
+//       <div className="flex flex-col items-center gap-4">
+//         <div className="p-6  rounded-lg shadow-md text-center">
+//           <h1 className="text-3xl font-bold mb-4">Not Found</h1>
+//           <p className="text-destructive">Could not find requested page</p>
+//           <Button
+//             variant="outline"
+//             className="mt-4 ml-2 "
+//             onClick={() => (window.location.href = "/")}
+//           >
+//             Back to Home
+//           </Button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default SnippetNotFoundPage;
+
+const NotFoundPage = () => {
+  return <>Not Found Page</>;
 };
 
-export default SnippetNotFoundPage;
+export default NotFoundPage;
